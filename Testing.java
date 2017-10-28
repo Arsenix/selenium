bdbe5f32c98041e38f307ae705f50e69611264b9
Pablo Joaquin Reyes Gonzalez <pablo.reyes@safecard.cl>
2017-10-28 19:36:12 -0300

    Se añaden los comentarios y se añade la parte IDE

--- Testing.java
+++ Testing.java
@@ -72,22 +72,26 @@ public class Testing {
     
     
     void Youtube() {
-        
+        // Se el buscador de youtube se escribe la cancion a buscar, posterior a esto se hace click en la lupa, buscador
+        // luego se selecciona la primera opcion para la busqueda realizada.
         driver.findElement(By.id("search")).sendKeys("sensual seduction");
         driver.findElement(By.id("search-icon-legacy")).click();
         driver.findElement(By.className("style-scope ytd-item-section-renderer")).click();
     }
     
     void Login(){
-        //Se realiza el login en backend.safecard.cl    
+        //Se realiza el login en backend.safecard.cl con telefono y contraseña de una de las promotoras   
+        //Se valida que la prueba sea valida con el hecho de que haga login de forma correcta.
         driver.findElement(By.id("mobile")).sendKeys("951789537");
         driver.findElement(By.id("password")).sendKeys("1234");
         driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div[5]/button")).click();
-        //wait = new WebDriverWait (driver,2);
     }
     
     void Cotizar(){
-        //Se realiza una busqueda sobre AstroGrow
+        //Se realiza una busqueda sobre DutchPassion y se realiza la busqueda en sobre la semilla Auto Xtreme
+        //posterior a esto se hace click en la busqueda y se selecciona el primer elemento de la busqueda, que corresponde a las semillas
+        //Se añade una semilla de 3 unidades al carro de compra y se finaliza la compra
+        //La prueba se valida con que se cumplan los pasos anteriormente descritos.
         driver.findElement(By.name("s")).sendKeys("Auto Xtreme");
         driver.findElement(By.id("zoekhet")).click();
         driver.findElement(By.xpath("//*[@id=\"kweekinfo\"]/div/div[1]/div[2]/p/a")).click();
@@ -99,101 +103,162 @@ public class Testing {
     }
     
     void Manuales(){
-        //se conecta y busca el manual de instalación.
-        driver.findElement(By.id("nav-tutoriales")).click();
-        driver.findElement(By.xpath("//*[@id=\"nav-docs-tutoriales\"]")).click();
-            
-    }
-    
-    void AgregarPropiedad(){
-        //Se agrega una nueva propiedad
-        driver.findElement(By.className("btn btn-gray btn-xs hidden-xs hidden-sm")).click();
-        driver.findElement(By.name("name")).sendKeys("Pruebas Automatizacion");
-        driver.findElement(By.className("class=\"btn dropdown-toggle bs-placeholder btn-default\"")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[3]/div/div/div/ul/li[1]/a/span[1]")).click();
-        driver.findElement(By.name("max_stock")).sendKeys("5"); //Se añade arbitariamente un número
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[5]/div/div/input")).sendKeys("1108");
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[6]/div/button")).click();
-        driver.findElement(By.cssSelector("input[type='text']")).click();
-        wait = new WebDriverWait (driver,2);   
-    
-    }
-    void ListadoPropiedades(){
-        driver.findElement(By.xpath("//*[@id=\"main-menu\"]/li[1]/a/span")).click();
-        wait = new WebDriverWait (driver,2);
-          
-    }
-    
-    void ListarResidentes(){
-        driver.findElement(By.xpath("//*[@id=\"main-menu\"]/li[2]/a/span")).click();
-        wait = new WebDriverWait (driver,2);
-        
-    }
-    
-    void EliminarResidente(){
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/table[2]/tbody/tr[5]/td[9]/div[1]/button/span[1]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/table[2]/tbody/tr[5]/td[9]/div[1]/ul/li[3]/a")).click();
-    
-    
-    }
-    
-    void AgregarResidente(){
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[1]/div[3]/div/div[1]/a[1]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[1]/div/div/button/span[1]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[1]/div/div/div/ul/li[1]/a")).click();
-        driver.findElement(By.name("mobile")).sendKeys("976357409");
-        driver.findElement(By.name("name")).sendKeys("Soporte");
-        driver.findElement(By.name("last_name")).sendKeys("Safecard");
-        //driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[5]/div/label[1]/span[2]")).click();
-        driver.findElement(By.name("submit")).click();
-        wait = new WebDriverWait (driver,2);
-         
-    }
-            
-   
-    void BuscarResidentes(){
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[4]/div/div/div/div[2]/table[1]/tbody/tr/td[1]/table/tbody/tr/td[1]/input")).sendKeys("Soporte");
-        driver.findElement(By.className("btn btn-orange btn-sm btn-buscar")).click();
-        //driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/table[1]/tbody/tr/td[1]/table/tbody/tr/td[2]/button")).click(); 
-        wait = new WebDriverWait (driver,2);
-    }
-    
-    
-    
-    void Buscar(){
-        driver.findElement(By.id("mobile")).sendKeys("976357409");
-        driver.findElement(By.id("password")).sendKeys("1234");
+        //Desde el footer de la pagina se selecciona el link de manuales se seleciona y busca el manual de instalación.
+        driver.findElement(By.xpath("form-control ng-pristine ng-empty ng-valid-email ng-invalid ng-invalid-required ng-touched")).sendKeys("pablo.reyes@safecard.cl");
         driver.findElement(By.id("submit")).click();
-        driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[2]/div/div[1]/a[1]")).click();
-        driver.findElement(By.id("name")).sendKeys("Pruebas");
-        driver.findElement(By.id("rol")).sendKeys("Automatizacion");
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[3]/div/div/button/span[1]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[3]/div/div/div/ul/li/a")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[6]/div/button")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[1]/div[3]/div/div[1]/a[1]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[1]/div/div/button/span[1]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/div/div/div/div[2]/form/div[1]/div/div[1]/div/ul/li[1]/a/span[1]")).click();
-        driver.findElement(By.name("mobile")).sendKeys("976357409");
-        driver.findElement(By.name("name")).sendKeys("PaBlo");
-        driver.findElement(By.name("last_name")).sendKeys("ReYeS");
-        driver.findElement(By.name("submit")).click(); 
-        
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[4]/div/div/div/div[2]/table[2]/tbody/tr[5]/td[9]/div[1]/button/span[1]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[3]/div[4]/div/div/div/div[2]/table[2]/tbody/tr[5]/td[9]/div[1]/ul/li[3]/a")).click();
-           
+            
     }
+    
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.InterruptedException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // TODO code application logic here
         Testing navegador = new Testing(); 
-        //navegador.AbrirYoutube();
+        navegador.AbrirYoutube();
         navegador.AbrirBackofficeSafecard();
         navegador.AbrirDutchPassion();
         navegador.AbrirSafecard();
 
     }
     
+// Archivos de Selenium IDE
+// Backoffice.safecard.cl    
+//  <?xml version="1.0" encoding="UTF-8"?>
+//<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+//<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+//<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+//<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+//<link rel="selenium.base" href="https://backoffice.safecard.cl/" />
+//<title>backoffice.safecard</title>
+//</head>
+//<body>
+//<table cellpadding="1" cellspacing="1" border="1">
+//<thead>
+//<tr><td rowspan="1" colspan="3">backoffice.safecard</td></tr>
+//</thead><tbody>
+//<tr>
+//	<td>open</td>
+//	<td>/</td>
+//	<td></td>
+//</tr>
+//<tr>
+//	<td>clickAndWait</td>
+//	<td>//button[@type='submit']</td>
+//	<td></td>
+//</tr>
+//</tbody></table>
+//</body>
+//</html>
+    
+//Safecard.cl
+//<?xml version="1.0" encoding="UTF-8"?>
+//<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+//<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+//<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+//<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+//<link rel="selenium.base" href="https://safecard.cl/" />
+//<title>safecard</title>
+//</head>
+//<body>
+//<table cellpadding="1" cellspacing="1" border="1">
+//<thead>
+//<tr><td rowspan="1" colspan="3">safecard</td></tr>
+//</thead><tbody>
+//<tr>
+//	<td>open</td>
+//	<td>/</td>
+//	<td></td>
+//</tr>
+//<tr>
+//	<td>click</td>
+//	<td>id=nav-prensa</td>
+//	<td></td>
+//</tr>
+//<tr>
+//	<td>click</td>
+//	<td>css=h2.ng-binding</td>
+//	<td></td>
+//</tr>
+//
+//</tbody></table>
+//</body>
+//</html>
+    
+//youtube.com    
+//<?xml version="1.0" encoding="UTF-8"?>
+//<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+//<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+//<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+//<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+//<link rel="selenium.base" href="https://www.youtube.com/" />
+//<title>youtube</title>
+//</head>
+//<body>
+//<table cellpadding="1" cellspacing="1" border="1">
+//<thead>
+//<tr><td rowspan="1" colspan="3">youtube</td></tr>
+//</thead><tbody>
+//<tr>
+//	<td>open</td>
+//	<td>/</td>
+//	<td></td>
+//</tr>
+//<tr>
+//	<td>type</td>
+//	<td>name=search_query</td>
+//	<td>volver</td>
+//</tr>
+//<tr>
+//	<td>click</td>
+//	<td>id=search-icon-legacy</td>
+//	<td></td>
+//</tr>
+//<tr>
+//	<td>click</td>
+//	<td>xpath=(//a[contains(text(),'SFDK - Volver')])[2]</td>
+//	<td></td>
+//</tr>
+
+//</tbody></table>
+//</body>
+//</html>
+
+//Dutch-Passion
+   
+    //<?xml version="1.0" encoding="UTF-8"?>
+//<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+//<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+//<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+//<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+//<link rel="selenium.base" href="https://www.dutch-passion.com/" />
+//<title>Dutch-passion</title>
+//</head>
+//<body>
+//<table cellpadding="1" cellspacing="1" border="1">
+//<thead>
+//<tr><td rowspan="1" colspan="3">Dutch-passion</td></tr>
+//</thead><tbody>
+//<tr>
+//	<td>open</td>
+//	<td>/en/</td>
+//	<td></td>
+//</tr>
+//<tr>
+//	<td>clickAndWait</td>
+//	<td>css=img[alt=&quot;Afghani Kush&quot;]</td>
+//	<td></td>
+//</tr>
+//<tr>
+//	<td>clickAndWait</td>
+//	<td>//body/div[4]/div/div/div[2]/a/div/div[2]</td>
+//	<td></td>
+//</tr>
+
+//</tbody></table>
+//</body>
+//</html>
+
 }
